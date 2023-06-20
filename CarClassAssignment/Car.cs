eeba2ceff25a99a3375ab72c86073ed93a1fc542
Nicholas Crump <113931857+Kataruse@users.noreply.github.com>
2023-06-20 19:19:09 -0400

    Add all files from Laptop

---
+++ CarClassAssignment/Car.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassAssignment
+{
+    internal class Car
+    {
+        //attributes of Car's
+        private string _make;
+        private string _model;
+        private int _year;
+        private double _maxTankLevel;
+        private double _currentTankLevel;
+        private double _milesPerGallon;
+
+        // below is getters and setters
+        public string getMake()
+        { 
+            return _make; 
+        }
+        public string getModel()
+        {
+            return _model;
+        }
+        public int getYear() 
+        {
+            return _year;
+        }
+        public double getMaxTankLevel()
+        {
+            return _maxTankLevel;
+        }
+        public double getCurrentTankLevel()
+        {
+            return _currentTankLevel;
+        }
+        public double getMilesPerGallon()
+        {
+            return _milesPerGallon;
+        }
+
+        public void setMake(string make)
+        {
+            _make = make;
+        }
+        public void setModel(string model)
+        {
+            _model = model;
+        }
+
+        public void setYear(int year)
+        {
+            if (year >= 1900 && year <= DateTime.Now.Year)
+            {
+                _year = year;
+            }
+            else
+            {
+                _year = 2018;
+            }
+        }
+
+        public void setMaxTankLevel(double maxTankLevel)
+        {
+            if (maxTankLevel > 0)
+            {
+                _maxTankLevel = maxTankLevel;
+            }
+            else
+            {
+                _maxTankLevel = 23;
+            }
+        }
+
+        public void setCurrentTankLevel(double currentTankLevel)
+        {
+            if (currentTankLevel > 0)
+            {
+                _currentTankLevel = currentTankLevel;
+            }
+            else
+            {
+                _currentTankLevel = 18.5;
+            }
+        }
+
+        public void setMilesPerGallon(double milesPerGallon)
+        {
+            if (milesPerGallon > 0)
+            {
+                _milesPerGallon = milesPerGallon;
+            }
+            else
+            {
+                _milesPerGallon = 21;
+            }
+        }
+        
+        // Car default constructor
+        public Car()
+        {
+            setMake("Ford");
+            setModel("F150");
+            setYear(2018);
+            setMaxTankLevel(23);
+            setCurrentTankLevel(18.5);
+            setMilesPerGallon(21);
+        }
+
+        // Car parameterized constuctor
+        public Car(string make, string model, int year, double maxTankLevel, double currentTankLevel, double milesPerGallon)
+        {
+            setMake(make);
+            setModel(model);
+            setYear(year);
+            setMaxTankLevel(maxTankLevel);
+            setCurrentTankLevel(currentTankLevel);
+            setMilesPerGallon(milesPerGallon);
+        }
+
+        // Car copy constuctor
+        public Car(Car existingCar)
+        {
+            setMake(existingCar._make);
+            setModel(existingCar._model);
+            setYear(existingCar._year);
+            setMaxTankLevel(existingCar._maxTankLevel);
+            setCurrentTankLevel(existingCar._currentTankLevel);
+            setMilesPerGallon(existingCar._milesPerGallon);
+        }
+
+        // method that takes the user input and adds it to the current tank if its greater than 0, and doesnt exceed the max tank level
+        public string FillGasTank(double gallonsToFill)
+        {
+            if (gallonsToFill > 0)
+            {
+                if (gallonsToFill + _currentTankLevel <= _maxTankLevel)
+                {
+                    setCurrentTankLevel(gallonsToFill);
+                    return $"{gallonsToFill} gallons was replenished to your tank.";
+                }
+                return $"{gallonsToFill} gallons is more than your max tank level or {_maxTankLevel} gallons.";
+            }
+            return $"{gallonsToFill} is less than 0.";
+        }
+
+        // method that checks to see if the distance put into the method can be driven
+        private bool CanDriveDistance(double distance)
+        {
+            if (distance <= Math.Round(_milesPerGallon*_currentTankLevel, 2) && distance > 0)
+            { 
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        // method that removes the correct amount of gas that is used to drive the distance that was input
+        public string Drive(double distance)
+        {
+            if (CanDriveDistance(distance))
+            {
+                _currentTankLevel = Math.Round(_currentTankLevel - distance / _milesPerGallon, 2);
+                return $"You drove {distance} miles and used {Math.Round(distance / _milesPerGallon, 2)} gallons of gas.\nYour tank is now at {_currentTankLevel} gallons.";
+            }
+            else
+            {
+                return $"Not a valid distance entry.\nEnter a distance less than or equal to {Math.Round(_currentTankLevel * _milesPerGallon, 2)} and greater than 0.";
+            }
+        }
+        
+        // method that displays the created Car's attributes and current fuel level
+        public string ToString()
+        {
+            return $"Make: {_make}\nModel: {_model}\nYear: {_year}\nMPG: {_milesPerGallon}\nCurrent Tank Level: {_currentTankLevel} out of {_maxTankLevel}";
+        }
+    }
+}
---
+++ CarClassAssignment/Program.cs
@@ -0,0 +1,214 @@
+﻿
+
+using System.Transactions;
+/**
+*--------------------------------------------------------------------
+* File name: CarClassAssignment
+* Project name: CarClassAssignment
+*--------------------------------------------------------------------
+* Author’s name and email: Nick Crump, CRUMPNA@ETSU.ED
+* Course-Section: CSCI 1260-002
+* Creation Date: 1/23/2023
+* -------------------------------------------------------------------
+*/
+namespace CarClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // below displays the default car class
+            Console.WriteLine("--- This is the default Car Class ---");
+            Car userCar = new Car();
+            Console.WriteLine(userCar.ToString());
+            Console.Write("Press Enter to Continue: ");
+            Console.ReadLine();
+            Console.Clear();
+            
+            // below prompts the user to create a car
+            Console.WriteLine("Enter your car's make: ");
+            userCar.setMake(Console.ReadLine());
+
+            Console.WriteLine("Enter your car's model: ");
+            userCar.setModel(Console.ReadLine());
+
+            Console.WriteLine($"Enter your car's year (1900 - {DateTime.Now.Year+1}): ");
+            try
+            {
+                userCar.setYear(Convert.ToInt32(Console.ReadLine()));
+            }
+            catch (Exception)
+            {
+                userCar.setYear(2018);
+            }
+
+            Console.WriteLine("Enter your car's tank size (in gallons): ");
+            double tank = 23;
+            try
+            {
+                tank = Convert.ToDouble(Console.ReadLine());
+                userCar.setMaxTankLevel(tank);
+                userCar.setCurrentTankLevel(tank);
+            }
+            catch (Exception)
+            {
+                userCar.setMaxTankLevel(tank);
+                userCar.setCurrentTankLevel(tank);
+            }
+
+            Console.WriteLine("Enter your car's mpg: ");
+            try
+            {
+                userCar.setMilesPerGallon(Convert.ToDouble(Console.ReadLine()));
+            }
+            catch (Exception)
+            {
+                userCar.setMilesPerGallon(21);
+            }
+
+            // below is a method to display the menu
+            static void DisplayMenu(Car car)
+            {
+                Console.WriteLine(car.ToString());
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("[1] Travel to Kingsport (25 miles)");
+                Console.WriteLine("[2] Travel to Knoxville (108 miles)");
+                Console.WriteLine("[3] Specify Distance to Travel");
+                Console.WriteLine("[4] Check on car details");
+                Console.WriteLine("[5] Fill Gas Tank");
+                Console.WriteLine("[0] Quit Program");
+                Console.WriteLine("-------------------------------------");
+            }
+
+            int menuInput = 0;
+            double miles = 0;
+            double gas = 0;
+            bool intCheck = false;
+            bool highCheck = false;
+            bool doubleCheck = false;
+            bool mileCheck = true;
+            bool gasCheck = true;
+
+            do
+            {
+                // below are error checks for menu inputs if the user input a number either too high or one which was not valid
+                Console.Clear();
+                if (intCheck == true)
+                {
+                    Console.WriteLine("* ERROR: Menu input was not an integer!");
+                }
+                if (highCheck == true)
+                {
+                    Console.WriteLine("* ERROR: Menu input was higher than expected!");
+                }
+                intCheck = false;
+                highCheck = false;
+
+                //below gets the users menu input
+                DisplayMenu(userCar);
+                try
+                {
+                    menuInput = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    menuInput = 6;
+                    intCheck = true;
+                }
+
+                switch (menuInput) 
+                {
+                    case 1:
+                        // below drives to Kingsport
+                        Console.Clear();
+                        Console.WriteLine(userCar.Drive(25));
+                        Console.Write("Press Enter to Continue: ");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        //below drives to Knoxville
+                        Console.Clear();
+                        Console.WriteLine(userCar.Drive(108));
+                        Console.Write("Press Enter to Continue: ");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        // below drives a user chosen distance
+                        do
+                        {
+                            // below gets user input & checks if the input was valid and repeats until it is
+                            try
+                            {
+                                Console.Clear();
+                                if (doubleCheck == true)
+                                {
+                                    Console.WriteLine("* ERROR: mile input was not a double!");
+                                }
+                                doubleCheck = false;
+                                Console.Write("Enter the Amount of Miles: ");
+                                miles = Convert.ToDouble(Console.ReadLine());
+                                mileCheck = false;
+                            }
+                            catch (Exception)
+                            {
+                                doubleCheck = true;
+                            }
+                        }
+                        while (mileCheck == true);
+                        mileCheck = true;
+                        Console.WriteLine(userCar.Drive(miles));
+                        Console.Write("Press Enter to Continue: ");
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        // below displays the created car's attributes
+                        Console.Clear();
+                        Console.WriteLine(userCar.ToString());
+                        Console.Write("Press Enter to Continue: ");
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        // below fills the tank by the amount of gallons the user choose
+                        do
+                        {
+                            // below gets user input & checks if the input was valid and repeats until it is
+                            try
+                            {
+                                Console.Clear();
+                                if (doubleCheck == true)
+                                {
+                                    Console.WriteLine("* ERROR: Gas input was not a double!");
+                                }
+                                doubleCheck = false;
+                                Console.Write("Enter the Amount of Gas: ");
+                                gas = Convert.ToDouble(Console.ReadLine());
+                                gasCheck = false;
+                            }
+                            catch (Exception)
+                            {
+                                doubleCheck = true;
+                            }
+                        }
+                        while (gasCheck == true);
+                        mileCheck = true;
+                        Console.WriteLine(userCar.FillGasTank(gas));
+                        Console.Write("Press Enter to Continue: ");
+                        Console.ReadLine();
+                        break;
+                }
+                // this checks to see if the intCheck was triggered and if not then it'll check to see if the input was higher than the max menu selection
+                if (intCheck == false)
+                {
+                    if (menuInput > 5)
+                    {
+                        highCheck = true;
+                    }
+                }
+            }
+            while (menuInput > 0);
+            Console.Clear();
+            Console.WriteLine("Program Closing...");
+
+        }
+    }
+}
